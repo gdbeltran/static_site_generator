58beab52a14bfb524c29c7702e913af3d65e6e1e
David Beltran <david.beltran@trojans.dsu.edu>
2025-04-02 18:26:31 -0500

    Finished moving tests around.

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -47,9 +47,19 @@ weird spaces.
                 "- item\n- thing\n- fidget\n-"
             ])
         
-    def test_is_ordered_list(self):
-        valid_block = "1. First item\n2. Second item\n3. Third item"
-        self.assertEqual(block_to_block_type(valid_block), BlockType.ORDERED_LIST)
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
         
 if __name__ == "__main__":
     unittest.main()
